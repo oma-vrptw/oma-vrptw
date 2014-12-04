944b8ff7f8f5ecbf7fbc1b4b4b26172b07b802bf
oma-vrptw <oma.vrptw@gmail.com>
2014-12-04 15:48:33 +0100

    updated code following Perboli tips: "in order to avoid rounding errors when dealing with the real costs in the objective function, please updare the method calculateDistance() in the file Instance.java"

--- src/com/mdvrp/Instance.java
+++ src/com/mdvrp/Instance.java
@@ -277,25 +277,28 @@ public class Instance {
 				//case both customers
 				if(i < customersNr && j < customersNr){
 					distances[i][j] = Math.sqrt(Math.pow(customers.get(i).getXCoordinate() - customers.get(j).getXCoordinate(), 2)
-										+ Math.pow(customers.get(i).getYCoordinate() - customers.get(j).getYCoordinate(), 2));
+							+ Math.pow(customers.get(i).getYCoordinate() - customers.get(j).getYCoordinate(), 2));
+					distances[i][j] = Math.floor(distances[i][j] * 10) / 10;
 					distances[j][i] = distances[i][j];
-					
-				// case customer and depot					
+
+					// case customer and depot
 				}else if(i < customersNr && j >= customersNr){
 					int d = j - customersNr; // depot number in the instance list
 					distances[i][j] = Math.sqrt(Math.pow(customers.get(i).getXCoordinate() - depots.get(d).getXCoordinate(), 2)
 							+ Math.pow(customers.get(i).getYCoordinate() - depots.get(d).getYCoordinate(), 2));
+					distances[i][j] = Math.floor(distances[i][j] * 10) / 10;
 					distances[j][i] = distances[i][j];
-				
-				// case both depots
+
+					// case both depots
 				}else if(i >= customersNr && j >= customersNr){
 					int d1 = i - customersNr; // first depot number in the instance list
 					int d2 = j - customersNr; // second depot number in the instance list
 					distances[i][j] = Math.sqrt(Math.pow(depots.get(d1).getXCoordinate() - depots.get(d2).getXCoordinate(), 2)
 							+ Math.pow(depots.get(d1).getYCoordinate() - depots.get(d2).getYCoordinate(), 2));
+					distances[i][j] = Math.floor(distances[i][j] * 10) / 10;
 					distances[j][i] = distances[i][j];
 				}
-			}		
+			}
 	}
 	
 	/**
